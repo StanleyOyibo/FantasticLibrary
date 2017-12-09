462978b2c96672c6b5fea20ee23b8caf7c580320
Stanley Oyibo <dtopstar@gmail.com>
2017-12-09 12:54:27 +0100

    Revert "Update Local to Remote Change"
    This reverts commit 64b008da3a3ed650698551993e18960a7de722b6.

--- FantasticLibrary/Class1.cs
+++ FantasticLibrary/Class1.cs
@@ -6,40 +6,7 @@ using System.Threading.Tasks;
 
 namespace FantasticLibrary
 {
-    public class FantasticClass : IDisposable
+    public class Class1
     {
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // ~FantasticClass() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        void IDisposable.Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
     }
 }
--- README.md
+++
@@ -1,5 +0,0 @@
-# FantasticLibrary
-
-## VS Sample repo
-
-Welcome to a new repo that will make you save a lot of time
